77121add0551c8a8555252c2d40a559b41140eb0
Arun Patil <arun26feb@gmail.com>
2020-09-11 21:04:26 -0400

    Update data_index.js

--- JS/data_index.js
+++ JS/data_index.js
@@ -23,7 +23,7 @@ new Chart(document.getElementById("bar-chart"), {
       datasets: [
         {
           label: "Population (millions)",
-          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+          backgroundColor: ["#3e95cd"],
           data: [2478,5267,734,784,433]
         }
       ]
