6d1950d02e2d5bf46ca8d2e670ed485922a63604
LoanerUser <loaneruser@PATH-CA-0JCGQ18.local>
2020-10-19 15:57:05 -0400

    Edited electron dependencies for Mac and Linux

--- src/Mac/miRge3.0/index.js
+++ src/Mac/miRge3.0/index.js
@@ -3,7 +3,7 @@ const {app, BrowserWindow} = electron
 
 
 app.on('ready',() => {
-	let win = new BrowserWindow({width:1400, height:600, icon: __dirname + './build/mirge_icon.png',
+	let win = new BrowserWindow({width:1400, height:800, icon: __dirname + './build/mirge_icon.png',
 	})
 	win.loadURL(`file://${__dirname}/index.html`)
 	//win.setMenu(null);
